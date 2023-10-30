d2a7c47a64f82d19cc75a18618788873514a3b79
carpa <carparias123@gamil.com>
2023-10-30 13:09:27 +0100

    .

---
+++ tensor_calculator/__init__ .py
@@ -0,0 +1,14 @@
+from tensor_calculator.Tensors_exercise import *
+
+__all__ = Tensors_exercise.__all__
+
+MAJOR = 1
+MINOR = 0
+PATCH = 0
+COMMIT = 12
+
+# from main import *
+
+__version__ = f'{MAJOR}.{MINOR}.{PATCH}.{COMMIT}'
+__author__  = 'Universidad Francisco de Vitoria'
+__name__    = 'module_structure'
